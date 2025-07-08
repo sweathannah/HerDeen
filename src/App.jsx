b7333129f9f64a39d1d37cdbacc837e85b5bd63d
sweathannah <sultanatmbashir@gmail.com>
2025-07-08 19:15:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react'
+import Home from './pages/Home.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+// src/pages/Home.jsx
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <section>
+        <header>
+            <nav>
+                <img src="/images/logo.png" alt="HerDeen Logo"  className='w-[5rem] h-[5rem]'/>
+                <ul>
+                    <li class="text-3xl font-bold underline">HerDeen</li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+
+        </main>
+      </section>
+    </>
+  )
+}
+
+export default Home
